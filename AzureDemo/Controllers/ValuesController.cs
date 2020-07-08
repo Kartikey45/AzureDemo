30e45d75da703d0a3af84e69f8e6934344c91f78
Kartikey45 <singhkartikey45@gmail.com>
2020-07-09 03:14:51 +0530

    second commit

--- AzureDemo/Controllers/ValuesController.cs
+++ AzureDemo/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace AzureDemo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Kartikey", "Singh" };
         }
 
         // GET api/values/5
